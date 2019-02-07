529ededd3549eed7624d1f2b0843de2a3c6ac4dd
Tim Roesner <13894518+timroesner@users.noreply.github.com>
2019-02-06 19:29:15 -0800

    Initialize Xcode project

---
+++ GPA TrackerTests/GPA_TrackerTests.swift
@@ -0,0 +1,34 @@
+//
+//  GPA_TrackerTests.swift
+//  GPA TrackerTests
+//
+//  Created by Tim Roesner on 2/6/19.
+//  Copyright © 2019 Tim Roesner. All rights reserved.
+//
+
+import XCTest
+@testable import GPA_Tracker
+
+class GPA_TrackerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
