834817645774c4efaae5aa18298d34a5676f6d5b
Tim Roesner <13894518+timroesner@users.noreply.github.com>
2019-02-06 19:35:14 -0800

    Setup root VC

--- GPA Tracker/AppDelegate.swift
+++ GPA Tracker/AppDelegate.swift
@@ -15,7 +15,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        window = UIWindow()
+        window?.makeKeyAndVisible()
+        let vc = MainViewController()
+        
+        window?.rootViewController = vc
+        
+        if ProcessInfo.processInfo.arguments.contains("UITests") {
+            UIApplication.shared.keyWindow?.layer.speed = 100
+        }
         return true
     }
 
