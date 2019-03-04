a06a1833128404e48850f30ee93484c85964d609
Aluoh <jiceredits@gmail.com>
2019-03-04 15:47:32 -0800

    Course class/model overview
    Course can have a name, weight, grade, and can be ignored. Also added test cases for them.

---
+++ Course.swift
@@ -0,0 +1,24 @@
+//
+//  Course.swift
+//  GPA Tracker
+//
+//  Created by Jason Li on 3/4/19.
+//  Copyright © 2019 Tim Roesner. All rights reserved.
+//
+
+import Foundation
+
+public class Course {
+    var name: String // Course name
+    var weight: Double // Course weight
+    var grade: String // Course letter grade, A-, A, A+, etc
+    var isIgnored: Bool // Ignore the grade temporarily or not consider it at all
+    
+    init(name: String, weight: Double, grade: String, isIgnored: Bool) { // Constructor
+        self.name = name
+        self.weight = weight
+        self.grade = grade
+        self.isIgnored = isIgnored
+    }
+}
+
