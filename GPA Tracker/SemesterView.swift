ff7dce64f77d47bc1ed1e473e6d94ad333e7904d
Maria Surmenok <mariasurmenok@gmail.com>
2019-03-12 12:53:16 -0700

    made shadow slightly lighter

--- GPA Tracker/SemesterView.swift
+++ GPA Tracker/SemesterView.swift
@@ -13,6 +13,12 @@ let semesterName = UITextField(frame: CGRect(x: 16, y: 12, width: 320, height: 3
 let semesterGPA = UILabel(frame: CGRect(x: 16, y: 54, width: 320, height: 22 ))
 let tableOfClasses = UITableView(frame: CGRect(x: 16, y: 107, width: 320, height: 400))
 let newClassButton = UIButton(frame: CGRect(x: 20, y: 300, width: 300, height: 65))
+// header for the table
+let headerView = UIView(frame: CGRect(x: 0, y: 50, width: 324, height: 22))
+let classNameLabel = UILabel(frame: CGRect(x: 0, y: 5, width:150, height:22))
+let weightLabel = UILabel(frame: CGRect(x: 192, y: 5, width:54, height:22))
+let gradeLabel = UILabel(frame: CGRect(x: 266, y: 5, width:47, height:22))
+
 
 class SemesterView: UIView {
     public override init(frame: CGRect = defaultRect) {
@@ -21,10 +27,9 @@ class SemesterView: UIView {
         self.layer.cornerRadius = 30
         // TODO: Shadow should be lighter
         self.layer.shadowOffset = CGSize(width: 0, height: 0)
-        self.layer.shadowOpacity = 0.6
+        self.layer.shadowOpacity = 0.5
         self.layer.shadowRadius = 12
         // TODO: Adjust the size and padding of elements, so they look nice on all screens
-        // TODO: Adjust font styles
         
         semesterName.placeholder = "Title"
         semesterName.font = UIFont.systemFont(ofSize: 28, weight: UIFont.Weight.semibold)
@@ -33,17 +38,13 @@ class SemesterView: UIView {
         semesterGPA.textAlignment = .left
         semesterGPA.font = UIFont.systemFont(ofSize: 20, weight: UIFont.Weight.semibold)
         
-        // Header to the table
-        let headerView = UIView(frame: CGRect(x: 0, y: 50, width: 324, height: 22))
-        let classNameLabel = UILabel(frame: CGRect(x: 0, y: 5, width:150, height:22))
+        // Header for the table
         classNameLabel.text = "Class name"
         classNameLabel.font = UIFont.systemFont(ofSize: 17, weight: UIFont.Weight.regular)
         
-        let weightLabel = UILabel(frame: CGRect(x: 192, y: 5, width:54, height:22))
         weightLabel.text = "Weight"
         weightLabel.font = UIFont.systemFont(ofSize: 17, weight: UIFont.Weight.regular)
         
-        let gradeLabel = UILabel(frame: CGRect(x: 266, y: 5, width:47, height:22))
         gradeLabel.text = "Grade"
         gradeLabel.font = UIFont.systemFont(ofSize: 17, weight: UIFont.Weight.regular)
         
@@ -51,18 +52,18 @@ class SemesterView: UIView {
         headerView.addSubview(weightLabel)
         headerView.addSubview(gradeLabel)
         
-        tableOfClasses.numberOfRows(inSection: 1)
+        tableOfClasses.numberOfRows(inSection: 6)
         tableOfClasses.tableHeaderView = headerView
         
         // TODO: newClassButton should be in the footer of the table
-        
+        // TODO: for newClassButton the plus sign "+" should be an image
         // Set styles for button 'Add class'
         newClassButton.setTitle("+ Add class", for: UIControl.State.normal)
         newClassButton.setTitleColor(.black, for: UIControl.State.normal)
         newClassButton.contentHorizontalAlignment = UIControl.ContentHorizontalAlignment.left
         newClassButton.titleLabel?.font = UIFont.systemFont(ofSize: 28, weight: UIFont.Weight.semibold)
         
-        //tableOfClasses.tableFooterView = newClassButton
+        //tableOfClasses.tableFooterView = newClassButton // doesn't work as expected <_<
         
         self.addSubview(semesterName)
         self.addSubview(semesterGPA)
