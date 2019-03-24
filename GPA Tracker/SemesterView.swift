f4c6f0d13f58ac9622b2b9d07a7a49235b312b33
Maria Surmenok <mariasurmenok@gmail.com>
2019-03-23 23:50:11 -0700

    adapted elements to different screen sizes

--- GPA Tracker/SemesterView.swift
+++ GPA Tracker/SemesterView.swift
@@ -10,19 +10,24 @@ import UIKit
 import SnapKit
 
 let semesterNameHeight: CGFloat = 34.0
+let semesterGPAHeight: CGFloat = 22.0
+let weightLabelWidth: CGFloat = 74.0
+let gradeLabelWidth: CGFloat = 47.0
+let tableHeaderHeight: CGFloat = 22.0
+let addClassButtonHeight: CGFloat = 41.0
+
 let tableHeaderFont = UIFont.systemFont(ofSize: 17, weight: UIFont.Weight.regular)
 let header1Font = UIFont.systemFont(ofSize: 28, weight: UIFont.Weight.semibold)
 let headler2Font = UIFont.systemFont(ofSize: 20, weight: UIFont.Weight.semibold)
 
 var semesterTitle: UITextField!
-var semesterGPA = UILabel(frame: CGRect(x: 16, y: 54, width: 320, height: 22 ))
-var tableOfClasses = UITableView(frame: CGRect(x: 16, y: 107, width: 320, height: 400))
-var newClassButton = UIButton(frame: CGRect(x: 0, y: 300, width: 300, height: 65))
-// header for the table
-var headerView = UIView(frame: CGRect(x: 0, y: 50, width: 324, height: 22))
-var classNameLabel = UILabel(frame: CGRect(x: 0, y: 5, width:150, height:22))
-var weightLabel = UILabel(frame: CGRect(x: 192, y: 5, width:54, height:22))
-var gradeLabel = UILabel(frame: CGRect(x: 266, y: 5, width:47, height:22))
+var semesterGPA: UILabel!
+var tableOfClasses: UITableView!
+var headerView: UIView!
+var classNameLabel: UILabel!
+var weightLabel: UILabel!
+var gradeLabel: UILabel!
+var addClassButton: UIButton!
 
 
 class SemesterView: UIView {
@@ -34,46 +39,10 @@ class SemesterView: UIView {
         self.layer.shadowOffset = CGSize(width: 0, height: 0)
         self.layer.shadowOpacity = 0.4
         self.layer.shadowRadius = 12
-        self.clipsToBounds = true
         
         setupSemesterTitle()
-
-        semesterGPA.text = "GPA: --"
-        semesterGPA.textAlignment = .left
-        semesterGPA.font = headler2Font
-        
-        // Header for the table
-        classNameLabel.text = "Class name"
-        classNameLabel.font = tableHeaderFont
-        
-        weightLabel.text = "Weight"
-        weightLabel.font = tableHeaderFont
-        
-        gradeLabel.text = "Grade"
-        gradeLabel.font = tableHeaderFont
-
-        headerView.addSubview(classNameLabel)
-        headerView.addSubview(weightLabel)
-        headerView.addSubview(gradeLabel)
-        
-        tableOfClasses.numberOfRows(inSection: 6)
-        tableOfClasses.tableHeaderView = headerView
-        
-        // TODO: for newClassButton the plus sign "+" should be an image
-        // Set styles for button 'Add class'
-        newClassButton.setTitle("+ Add class", for: UIControl.State.normal)
-        newClassButton.setTitleColor(.black, for: UIControl.State.normal)
-        newClassButton.contentHorizontalAlignment = UIControl.ContentHorizontalAlignment.left
-        newClassButton.titleLabel?.font = header1Font
-        //tableOfClasses.insertSubview(newClassButton, at: 1)
-        
-        tableOfClasses.tableFooterView = newClassButton
-        
-        
-        self.addSubview(semesterGPA)
-        self.addSubview(tableOfClasses)
-        
-
+        setupSemesterGPA()
+        setupTable()
     }
     
     func setupSemesterTitle() {
@@ -86,6 +55,99 @@ class SemesterView: UIView {
             make.left.equalTo(self).offset(16)
             make.right.equalTo(self).offset(-16)
             make.top.equalTo(self).offset(12)
+        }
+    }
+    
+    func setupSemesterGPA() {
+        semesterGPA = UILabel()
+        semesterGPA.text = "GPA: --"
+        semesterGPA.textAlignment = .left
+        semesterGPA.font = headler2Font
+        self.addSubview(semesterGPA)
+        semesterGPA.snp.makeConstraints { (make) -> Void in
+            make.height.equalTo(semesterGPAHeight)
+            make.left.equalTo(self).offset(16)
+            make.right.equalTo(self).offset(-16)
+            make.top.equalTo(self).offset(54)
+        }
+    }
+    
+    func setupTable() {
+        tableOfClasses = UITableView()
+        setupTableHeader()
+        
+        tableOfClasses.numberOfRows(inSection: 6)
+        
+        
+        // TODO: for newClassButton the plus sign "+" should be an image
+        // Set styles for button 'Add class'
+        
+        addClassButton = UIButton()
+        addClassButton.setTitle("+ Add class", for: UIControl.State.normal)
+        addClassButton.setTitleColor(.black, for: UIControl.State.normal)
+        addClassButton.contentHorizontalAlignment = UIControl.ContentHorizontalAlignment.left
+        addClassButton.titleLabel?.font = header1Font
+        
+        let buttonImage = UIImage(named: "add)
+
+        tableOfClasses.tableFooterView = addClassButton
+        
+        tableOfClasses.addSubview(addClassButton)
+        // = UIButton(frame: CGRect(x: 0, y: 300, width: 300, height: 65))
+        addClassButton.snp.makeConstraints { (make) -> Void in
+            make.height.equalTo(addClassButtonHeight)
+            make.width.equalTo(tableOfClasses)
+        }
+        
+        self.addSubview(tableOfClasses)
+        
+        tableOfClasses.snp.makeConstraints { (make) -> Void in
+            make.left.equalTo(self).offset(16)
+            make.right.equalTo(self).offset(-8)
+            make.top.equalTo(self).offset(107)
+            make.bottom.equalTo(self).offset(-20)
+            
+        }
+    }
+    
+    func setupTableHeader() {
+        headerView = UIView()
+        
+        classNameLabel = UILabel()
+        classNameLabel.text = "Class name"
+        classNameLabel.font = tableHeaderFont
+        
+        weightLabel = UILabel()
+        weightLabel.text = "Weight"
+        weightLabel.font = tableHeaderFont
+        
+        gradeLabel = UILabel()
+        gradeLabel.text = "Grade"
+        gradeLabel.font = tableHeaderFont
+        
+        headerView.addSubview(classNameLabel)
+        headerView.addSubview(weightLabel)
+        headerView.addSubview(gradeLabel)
+        
+        classNameLabel.snp.makeConstraints { (make) -> Void in
+            make.left.equalTo(headerView)
+            make.right.equalTo(headerView).offset(-(weightLabelWidth + gradeLabelWidth))
+        }
+        
+        weightLabel.snp.makeConstraints { (make) -> Void in
+            make.right.equalTo(headerView).offset(-gradeLabelWidth)
+            make.width.equalTo(weightLabelWidth)
+        }
+        
+        gradeLabel.snp.makeConstraints { (make) -> Void in
+            make.right.equalTo(headerView)
+            make.width.equalTo(gradeLabelWidth)
+        }
+
+        tableOfClasses.tableHeaderView = headerView
+        headerView.snp.makeConstraints { (make) -> Void in
+            make.height.equalTo(tableHeaderHeight)
+            make.width.equalTo(tableOfClasses)
         }
     }
     
