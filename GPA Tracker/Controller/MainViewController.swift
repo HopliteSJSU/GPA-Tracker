32bab0d6a88afd20ff0ee025f589bc1d9a0b619c
Maria Surmenok <mariasurmenok@gmail.com>
2019-03-23 22:18:55 -0700

    moved semester view initialization to a separate function in main controller

--- GPA Tracker/Controller/MainViewController.swift
+++ GPA Tracker/Controller/MainViewController.swift
@@ -9,16 +9,23 @@
 import UIKit
 import SnapKit
 
+var semesterView: UIView!
+
+let bgColor = UIColor(displayP3Red: 253 / 255, green: 219 / 255, blue: 93 / 255, alpha: 1.0)
+
 class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = UIColor(displayP3Red: 253 / 255, green: 219 / 255, blue: 93 / 255, alpha: 1.0)
+        self.view.backgroundColor = bgColor
         
-        let semester = SemesterView()
-        self.view.addSubview(semester)
-        
-        semester.snp.makeConstraints { (make) -> Void in
+        setupSemesterView()
+    }
+    
+    func setupSemesterView() {
+        semesterView = SemesterView()
+        self.view.addSubview(semesterView)
+        semesterView.snp.makeConstraints { (make) -> Void in
             make.edges.equalTo(self.view).inset(UIEdgeInsets(top: 121, left: 19, bottom: 90, right: 19))
         }
     }
