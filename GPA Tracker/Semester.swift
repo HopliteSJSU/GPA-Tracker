a2f4732e67427397bef3c4bcff7bf0a782316ac5
Maria Surmenok <mariasurmenok@gmail.com>
2019-04-13 14:27:22 -0700

    added semester model to create mock data for semester view

---
+++ GPA Tracker/Semester.swift
@@ -0,0 +1,21 @@
+//
+//  Semester.swift
+//  GPA Tracker
+//
+//  Created by Mariia Surmenok on 4/13/19.
+//  Copyright © 2019 Tim Roesner. All rights reserved.
+//
+
+import Foundation
+
+public class Semester {
+    var name: String // Semester name
+    var gpa: Double // Semester gpa
+    var classes: [Course] // list of all classes taken this semester
+    
+    init(name: String, gpa: Double) {
+        self.name = name
+        self.gpa = gpa
+        self.classes = [Course]()
+    }
+}
