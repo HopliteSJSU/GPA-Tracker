d97218ffbd2ebf2d2ffe628ec348ff6c155623c6
Aluoh <jiceredits@gmail.com>
2019-04-27 17:03:09 -0700

    Fixed calculations for update current data
    Also now includes round up

--- GPA Tracker/OverallGPA.swift
+++ GPA Tracker/OverallGPA.swift
@@ -10,14 +10,19 @@ import Foundation
 
 public class OverallGPA {
     // By default, always construct it as 0
+    var nonRoundedCumulativeGPA: Double
     var cumulativeGPA: Double
+    var allSemesters: [tempSemester]
     init() {
         cumulativeGPA = 0
+        allSemesters = []
+        nonRoundedCumulativeGPA = 0
     }
     
     // Need a label to display Overall GPA
     
     func calcCumulativeGPA(semesters: [tempSemester]) {
+        
         if semesters.count == 1 {
             cumulativeGPA = semesters[0].gpa
         }
@@ -25,15 +30,20 @@ public class OverallGPA {
             var sumOfSemesterGPA: Double = 0
             // Adds each gpa value from semester GPA to the overall GPA sum,
             for semester in semesters {
+                allSemesters.append(semester)
                 sumOfSemesterGPA += semester.gpa
             }
-            cumulativeGPA = sumOfSemesterGPA / Double(semesters.count)
+            nonRoundedCumulativeGPA = sumOfSemesterGPA / Double(semesters.count)
+            let rounded = (nonRoundedCumulativeGPA * 100).rounded() / 100
+            cumulativeGPA = rounded
         }
     }
     
     func updateGPAFromCurrentData(currentSem: tempSemester) {
         // Iterate through table view, grab GPA, update it with cumulative GPA
         // The tempSemester itself should have a auto calculated GPA
-        cumulativeGPA = (cumulativeGPA + currentSem.gpa) / 2
+        nonRoundedCumulativeGPA = (nonRoundedCumulativeGPA + currentSem.gpa) / (Double(allSemesters.count) + 1)
+        let rounded = (nonRoundedCumulativeGPA * 100).rounded() / 100
+        cumulativeGPA = rounded
     }
 }
