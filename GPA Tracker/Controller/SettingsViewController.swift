0ca32e94c24cf34f6aa25980d1fe66f65806b97f
Aluoh <jiceredits@gmail.com>
2019-05-01 16:52:43 -0700

    Settings viewable page + done button

--- GPA Tracker/Controller/SettingsViewController.swift
+++ GPA Tracker/Controller/SettingsViewController.swift
@@ -12,8 +12,14 @@ import QuickTableViewController
 
 class SettingsViewController: QuickTableViewController {
     
+    let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(doneTap))
+    var navBar: UINavigationBar!
+    var navBarController: UINavigationController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        //self.tableView.contentInset = UIEdgeInsets(top: 44,left: 0,bottom: 0,right: 0);
+        // Make the tableview under the navigation bar
         tableContents = [
             Section(title: "Settings", rows: [
                 NavigationRow(text: "Grade Points", detailText: .value1(""), action: { [weak self] in self?.gradePoints($0) }),
@@ -31,7 +37,11 @@ class SettingsViewController: QuickTableViewController {
                 NavigationRow(text: "Support", detailText: .value1(""), action: { [weak self] in self?.support($0) }),
                 ]),
         ]
+        navBarController = UINavigationController(rootViewController: SettingsViewController())
+        // setupDoneButton()
+        navBarConfig()
     }
+    
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         super.tableView(tableView, didSelectRowAt: indexPath)
@@ -40,26 +50,52 @@ class SettingsViewController: QuickTableViewController {
 
     private func clearData (_ sender: Row) {
         // Clearing all data
+        print("Clear Data")
     }
     
     private func gradePoints (_ sender: Row) {
         // Sets the grade points user specify
+        print("GPA")
     }
     
     private func yourData (_ sender: Row) {
         // MyGPA and your data lorem ipsem?
+        print("YourData")
     }
     
     private func privacy (_ sender: Row) {
         // Privacy
+        print("Privacy")
     }
     
     private func acknowledge (_ sender: Row) {
         // Acknowledgements
+        print("Acknowledgements")
     }
     
     private func support (_ sender: Row) {
         // Support
+        print("Support")
+    }
+    
+    @objc func doneTap(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    public func setupNavBar() {
+      //  navBar = UINavigationBar(frame: CGRect(x: 0, y: 25, width: 100, height: 100))
+        // navBar.isTranslucent = false
+       //  view.addSubview(navBar)
+    }
+   // public func setupDoneButton() {
+   //     doneButton = UIButton(frame: CGRect(x: 20, y: 20, width: 20, height: 20))
+  //      doneButton.setImage(UIImage(named: "settingsButton"), for: .normal)
+    //    view.addSubview(doneButton)
+    //}
+    
+    public func navBarConfig() {
+        navigationItem.title = "MyGPA"
+        navigationItem.rightBarButtonItem = doneButton
     }
     
     
